9f21041ece63d8ce59626242e51295556f30cb48
Pranshu Singh Bisht <106018070+pranshubisht9@users.noreply.github.com>
2022-12-22 13:57:33 +0530

    my first maven-hibernate config

---
+++ Hibernate/First Maven Hibernate/src/main/java/App.java
@@ -0,0 +1,28 @@
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Project started.." );
+
+        //to check my hibernate configuration is correct or not so we will make an object
+
+
+//        SessionFactory is an interface -> (it is like a connection) -> one project , one session factory
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
+        System.out.println(factory);
+
+//        factory.getCurrentSession();
+
+
+
+    }
+}
